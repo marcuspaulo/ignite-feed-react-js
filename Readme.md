c97cc51f1581d9f68f5938296f414a67dba3618f
Marcus Paulo <mpaulobr@gmail.com>
2022-09-15 01:28:16 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,29 @@
+
+
+
+
+
+## Icons
+- [Phosphoricons](https://phosphoricons.com)
+
+### Install Phosphoricons
+https://github.com/phosphor-icons/phosphor-home#react
+
+Command
+```sh
+npm i phosphor-react
+```
+
+```jsx
+import { PencilLine } from 'phosphor-react';
+
+<footer className={styles.footer}>
+    <PencilLine />
+    <a href="#">Editar seu perfil</a>
+</footer>
+```
+
+### Adicionando um espaço em branco
+```jsx
+    <a href="#">http://localhost</a>{'  '}<a href="#">http://localhost:3000</a>
+```
---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                <img
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/2917521?v=4" />
+                    <div className={styles.authorInfo}>
+                        <strong>Marcus Paulo</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="14 de Setembro de 2022" dateTime="2002-09-14">Publicado a </time>
+            </header>
+
+            <div className={styles.content}>
+                <p>lorem ipsum dolor sit amet, consectetur</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <p>Voluptatibus, explicabo! Deserunt voluptates velit nisi tenetur consequatur architecto. Saepe magni voluptates quia commodi enim, fugiat facere. Veritatis quos veniam soluta obcaecati!</p>
+                <p> Teste espaço: lorem ipsum dolor sit amet, consectetur</p>
+                <a href="#">http://localhost</a>{'  '}<a href="#">http://localhost:3000</a>
+            </div>
+        </article>
+    )
+}
